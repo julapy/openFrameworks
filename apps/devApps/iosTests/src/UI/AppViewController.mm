4c8e142bccc563884894752e1b78504784eddcb7
lukasz karluk <julapy@googlemail.com>
2015-08-25 09:45:25 +1000

    added table view.

--- apps/devApps/iosTests/src/UI/AppViewController.mm
+++ apps/devApps/iosTests/src/UI/AppViewController.mm
@@ -8,7 +8,35 @@
 
 #import "AppViewController.h"
 
+@interface AppViewController() <UITableViewDelegate, UITableViewDataSource> {
+    IBOutlet UITableView * tableView;
+    NSArray * tableData;
+}
+@end
+
 @implementation AppViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
 
 @end
